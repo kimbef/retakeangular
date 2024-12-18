6f9d4c0c0cd1ad6a92bd3856db3108d02eed4480
kimbeff <kimbeff@gmail.com>
2024-12-18 20:10:51 +0200

    fixing some issues
    had some issues

--- angu-blog/src/app/pages/create/create.component.ts
+++ angu-blog/src/app/pages/create/create.component.ts
@@ -6,24 +6,27 @@ import { AuthService } from '../../services/auth.service';
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
+  styleUrls: ['./create.component.css'],
 })
 export class CreateComponent {
   createForm: FormGroup;
   userId: string | null = null;
+  errorMessage: string = '';
 
   constructor(private fb: FormBuilder, private postService: PostService, private auth: AuthService) {
     this.createForm = this.fb.group({
-      title: ['', Validators.required],
-      content: ['', Validators.required],
+      title: ['', [Validators.required, Validators.pattern(/^\w+$/)]], // Single word validation
+      content: ['', [Validators.required, Validators.minLength(50)]], // Minimum 50 characters
     });
 
     this.auth.getUser().subscribe((user) => {
-      this.userId = user?.uid || null; // Safely access the UID
+      this.userId = user?.uid || null;
     });
   }
 
   onSubmit(): void {
-    if (this.createForm.invalid || !this.userId) {
+    if (this.createForm.invalid) {
+      this.errorMessage = 'There are empty fields!';
       return;
     }
 
@@ -35,7 +38,7 @@ export class CreateComponent {
 
     this.postService.createPost(newPost).then(() => {
       this.createForm.reset();
+      this.errorMessage = ''; // Clear errors on success
     });
   }
 }
-
